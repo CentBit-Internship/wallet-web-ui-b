f2b890277ac9d9a5d0da769f7d081c28f0ae9c55
DhanteyUD <otseclinton60@gmail.com>
2022-07-31 18:43:05 +0100

    Total Component

--- src/components/styles/TotalContainer.styled.ts
+++ src/components/styles/TotalContainer.styled.ts
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 
 export const TotalContainer = styled.div`
   width: 60%;
-  margin-left: 5%;
+  margin-left: 3%;
   margin-top: 10%;
   display: flex;
   flex-direction: row;
-  background-color: #ff0000;
+  justify-content: space-evenly;
 `;
