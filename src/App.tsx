927e06362d0f777120b62233c4493059bd35a6ad
DhanteyUD <otseclinton60@gmail.com>
2022-07-31 18:28:02 +0100

    App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import {Container} from "./components/styles/Container.styled";
-import {Sidebar} from "./components/styles/Sidebar.styled";
-import {Dashboard} from "./components/styles/Dashboard.styled";
+import { Container } from "./components/styles/Container.styled";
+import { Sidebar } from "./components/styles/Sidebar.styled";
+import { Dashboard } from "./components/styles/Dashboard.styled";
+import Totals from "./components/Totals";
 
 function App() {
   return (
     <>
-    <Container>
-      <Sidebar>
-        <h1>Hello</h1>
-      </Sidebar>
-      <Dashboard>
-        <h1>Hi</h1>
-      </Dashboard>
-    </Container>
+      <Container>
+        <Sidebar>
+          <h1>Sidebar</h1>
+        </Sidebar>
+        <Dashboard>
+          <Totals />
+        </Dashboard>
+      </Container>
     </>
   );
 }
