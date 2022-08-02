5899125810cd792a05fe2e51afcd6b01a120f576
DhanteyUD <otseclinton60@gmail.com>
2022-08-02 18:48:57 +0100

    Table + Container

---
+++ src/components/styles/TableContainer.styled.ts
@@ -0,0 +1,21 @@
+/** @format */
+
+import styled from 'styled-components';
+
+export const TableContainer = styled.div`
+  position: relative;
+  width: 57%;
+  border-radius: 10px;
+  height: 34%;
+  margin-left: 4.5%;
+  margin-top: 1.5%;
+  border: 2px solid #e6e6e6;
+
+  h1 {
+    position: absolute;
+    font-size: 17px;
+    font-weight: 600;
+    left: 3%;
+    top: 8%;
+  }
+`;
