33db8fa53052960972454d5210fe90d5079c1957
DhanteyUD <otseclinton60@gmail.com>
2022-08-02 18:49:32 +0100

    Graph Container

--- src/components/styles/GraphContainer.styled.ts
+++ src/components/styles/GraphContainer.styled.ts
@@ -3,10 +3,19 @@
 import styled from 'styled-components';
 
 export const GraphContainer = styled.div`
+  position: relative;
   width: 57%;
   border-radius: 10px;
   height: 33.5%;
   margin-left: 4.5%;
   margin-top: 1.5%;
   border: 2px solid #e6e6e6;
+
+  h1 {
+    position: absolute;
+    font-size: 17px;
+    font-weight: 600;
+    left: 3%;
+    top: 8%;
+  }
 `;
