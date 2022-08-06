42422bf28b7348b4121756f1932dbe9cd0fe8e5e
DhanteyUD <otseclinton60@gmail.com>
2022-08-06 14:56:59 +0100

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
-import "./App.css";
-import BarChart from "./components/Activities/BarChart";
-import Earnings from "./components/Earnings";
-import { Container } from "./components/styles/Container.styled";
-import { Sidenav } from "./components/styles/Sidenav.styled";
-import { Dashboard } from "./components/styles/Dashboard.styled";
-import Totals from "./components/Totals";
-import SalesAnalytics from "./components/SalesAnalytics";
+/** @format */
+
+import './App.css';
+import BarChart from './components/Activities/BarChart';
+import Earnings from './components/Earnings';
+import { Container } from './components/styles/Container.styled';
+import { Sidenav } from './components/styles/Sidenav.styled';
+import { Dashboard } from './components/styles/Dashboard.styled';
+import Totals from './components/Totals';
+import SalesAnalytics from './components/SalesAnalytics';
 import SellingProducts from './components/SellingProducts';
 
 function App() {
   const barchartData = [
     {
-      title: "Mon",
+      title: 'Mon',
       value: 50,
     },
     {
-      title: "Tue",
-      value: 60,
-    },
-    {
-      title: "Wed",
-      value: 34,
-    },
-    {
-      title: "Thu",
-      value: 10,
-    },
-    {
-      title: "Fri",
-      value: 37,
-    },
-    {
-      title: "Sat",
-      value: 50,
-    },
-    {
-      title: "Sun",
+      title: 'Tue',
       value: 80,
+    },
+    {
+      title: 'Wed',
+      value: 15,
+    },
+    {
+      title: 'Thu',
+      value: 25,
+    },
+    {
+      title: 'Fri',
+      value: 75,
+    },
+    {
+      title: 'Sat',
+      value: 55,
+    },
+    {
+      title: 'Sun',
+      value: 35,
     },
   ];
   return (
@@ -47,15 +49,16 @@ function App() {
           <Totals />
           <SalesAnalytics />
           <SellingProducts />
+
+          <div className="App">
+            <header className="App-header">
+              <Earnings />
+            </header>
+            <div className="customer-activities">
+              <BarChart data={barchartData} />
+            </div>
+          </div>
         </Dashboard>
-        <div className="App">
-        <header className="App-header">
-          <Earnings />
-        </header>
-        <div className="customer-activities">
-          <BarChart data={barchartData} />
-        </div>
-      </div>
       </Container>
     </>
   );
