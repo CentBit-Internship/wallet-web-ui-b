1b4ecd6c3eab0b6a2d6741a17dffdab89256fc88
DhanteyUD <otseclinton60@gmail.com>
2022-08-06 14:56:38 +0100

    Graph data

---
+++ src/components/Graph/GraphData.tsx
@@ -0,0 +1,51 @@
+/** @format */
+
+export const series: ApexAxisChartSeries | ApexNonAxisChartSeries | undefined =
+  [
+    {
+      name: 'Offline sales',
+      data: [50, 200, 400, 250, 400, 600, 770],
+    },
+    {
+      name: 'Online sales',
+      data: [220, 600, 780, 670, 580, 350, 400],
+    },
+  ];
+
+export const options: ApexCharts.ApexOptions | undefined = {
+  chart: {
+    height: 350,
+    type: 'line',
+  },
+  colors: ['#a88ed4', '#b9e303'],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth',
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+  },
+  tooltip: {
+    x: {
+      format: 'MM',
+    },
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'center',
+    floating: true,
+    fontSize: "11px",
+    fontFamily: "Poppins, Arial",
+    fontWeight: 600,
+    offsetY: 0,
+    offsetX: 10,
+    markers: {
+      width: 8,
+      height: 8,
+      strokeWidth: 0,
+      radius: 12
+    },
+  }
+};
